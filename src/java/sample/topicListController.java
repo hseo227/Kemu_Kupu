c3689bb4c9898999e8b8f854593947b4656f5f2a
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-09-18 15:24:53 +1200

    Copy the statisticsController code and then modify it

--- src/java/sample/topicListController.java
+++ src/java/sample/topicListController.java
@@ -1,6 +1,85 @@
 package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 public class topicListController implements Initializable {
+    private ArrayList<Topic> topicList = new ArrayList<>();
+
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private Label mainLabel;
+    @FXML
+    private Button selectBtn;
+    @FXML
+    private TableView<Topic> table;
+    @FXML
+    private TableColumn<Topic, String> topicListColumn;
+
+
+    // the method get all the topics' name and then set them into the table
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            // get all the topics, the filename is the name of the topic
+            String command = "ls words";
+            ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+
+            Process process = pb.start();
+
+            BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            int exitStatus = process.waitFor();
+
+            if (exitStatus == 0) {
+                // get the name of the topic (filename) and then add it into the list
+                String name;
+                while ((name = stdout.readLine()) != null) {
+                    topicList.add(new Topic(name));
+                }
+
+            } else {  // otherwise print out the error
+                String line;
+                while ((line = stderr.readLine()) != null) {
+                    System.err.println(line);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // setting up the table and the column
+        topicListColumn.setCellValueFactory(new PropertyValueFactory<Topic, String>("name"));
+        table.getItems().setAll(topicList);
+    }
+
+    @FXML
+    private void selectTopic(ActionEvent event) throws IOException {
+        try {
+            // if the user selected a topic, then it will continue, otherwise it will throw an exception
+            Topic selectedTopic = table.getSelectionModel().getSelectedItem();
+            SpellingQuiz.setTopic(selectedTopic.getName());
+
+        } catch (Exception e) {
+            // if no topic is selected, then the default topic (1st topic) is selected
+            SpellingQuiz.setTopic(topicList.get(0).getName());
+        }
+
+        // after the topic is selected, time to do the quiz
+        SceneController.goToQuiz();
     }
 }
