67a01473d1fb921df7923989df8fb74fd951197b
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-09-18 15:21:54 +1200

    Changed the name of the class Topic

---
+++ src/java/sample/Topic.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Topic {
+    private final String name;
+
+    public Topic(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
