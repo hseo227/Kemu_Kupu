e1971aa325587324126a29c5ab5e085fdf4a6637
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-09-25 04:41:25 +1200

    quiz, change to GridPane from anchorpane

--- src/java/sample/quizController.java
+++ src/java/sample/quizController.java
@@ -12,8 +12,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 import javafx.util.StringConverter;
 
@@ -29,7 +30,7 @@ public class quizController implements Initializable {
 
 
     @FXML
-    private AnchorPane rootPane;
+    private GridPane rootPane;
     @FXML
     private Label mainLabel, promptLabel, userScore;
     @FXML
@@ -44,6 +45,8 @@ public class quizController implements Initializable {
     private ToggleButton togSpdSlider;
     @FXML
     private HBox macronsBtnsHBox;
+    @FXML
+    private VBox inputVBox1, inputVBox2;
 
 
     @Override
