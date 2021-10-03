09cccf91223b75242254ec89722f3b1f2997d7a4
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-10-04 02:16:30 +1300

    topicList, change back to AnchorPane

--- src/java/sample/topicListController.java
+++ src/java/sample/topicListController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,7 +21,7 @@ public class topicListController implements Initializable {
     private ArrayList<Topic> topicList = new ArrayList<>();
 
     @FXML
-    private GridPane rootPane;
+    private AnchorPane rootPane;
     @FXML
     private Label mainLabel;
     @FXML
