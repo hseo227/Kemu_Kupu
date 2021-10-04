42f6fdb984207bb8cc19a4e6fe3530cc28b38822
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-10-04 18:06:10 +1300

    Change quit button to image

--- src/java/sample/mainMenuController.java
+++ src/java/sample/mainMenuController.java
@@ -3,21 +3,35 @@ package sample;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class mainMenuController {
+public class mainMenuController implements Initializable {
 
     @FXML
     private AnchorPane rootPane;
     @FXML
     private Label mainMenuLabel;
     @FXML
-    private Button gamesModule, quit;
+    private Button practiseModule, gamesModule;
+    @FXML
+    private ImageView quitImg;
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // The text when the mouse hover on the quit image
+        Tooltip tooltip = new Tooltip("Click to quit the game");
+        Tooltip.install(quitImg, tooltip);
+    }
 
     // load New Spelling Quiz
     @FXML
