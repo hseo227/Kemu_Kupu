fa5bf31040bf28616ad8eac1dbeea5b4e7ea3d36
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-10-10 15:40:45 +1300

    Update and refactor module games fxml and class file

--- src/java/sample/moduleGamesController.java
+++ src/java/sample/moduleGamesController.java
@@ -29,7 +29,7 @@ public class moduleGamesController implements Initializable {
     @FXML
     private AnchorPane rootPane;
     @FXML
-    private Label mainLabel, promptLabel, userScore, macronsHelper, numOfLettersLabel;
+    private Label mainLabel, promptLabel, userScore, numOfLettersLabel;
     @FXML
     private TextField inputField;
     @FXML
@@ -41,7 +41,7 @@ public class moduleGamesController implements Initializable {
     @FXML
     private HBox macronsBtnsHBox;
     @FXML
-    private VBox inputVBox1, inputVBox2;
+    private VBox inputVBox;
 
 
     @Override
@@ -87,9 +87,7 @@ public class moduleGamesController implements Initializable {
 
         // otherwise, continue the game
         startBtn.setVisible(false);
-        inputVBox1.setVisible(true);
-        inputVBox2.setVisible(true);
-        playbackBtn.setVisible(true);
+        inputVBox.setVisible(true);
 
         newQuestion();
     }
@@ -246,14 +244,10 @@ public class moduleGamesController implements Initializable {
 
     // this method is to pause before each new question, also while pausing it disables the quiz related utilities
     private void pauseBetweenEachQ() {
-        inputVBox1.setDisable(true);
-        inputVBox2.setDisable(true);
-        playbackBtn.setDisable(true);
+        inputVBox.setDisable(true);
 
         pause.setOnFinished(e -> {
-            inputVBox1.setDisable(false);
-            inputVBox2.setDisable(false);
-            playbackBtn.setDisable(false);
+            inputVBox.setDisable(false);
 
             newQuestion();
         });
