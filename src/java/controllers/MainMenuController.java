be402ab893ac5cddd570adf0119e42ade3f4f82a
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-10-13 16:10:05 +1300

    Eliminate 'warnings' in mainMenu

--- src/java/controllers/MainMenuController.java
+++ src/java/controllers/MainMenuController.java
@@ -1,37 +1,25 @@
 package controllers;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import spellingQuiz.Module;
 import spellingQuizUtil.ModuleType;
-
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
 public class MainMenuController {
 
-    @FXML
-    private AnchorPane rootPane;
-    @FXML
-    private Label mainMenuLabel, mainMenuLabel1;
-    @FXML
-    private Button practiseModule, gamesModule, quitBtn;
-
 
     // load Practise Module
     @FXML
-    private void loadPractiseModule(ActionEvent event) throws IOException {
+    private void loadPractiseModule() throws IOException {
         Module.setModuleType(ModuleType.practise);
         SceneController.goToTopicList();
     }
 
     // load Games Module
     @FXML
-    private void loadGamesModule(ActionEvent event) throws IOException {
+    private void loadGamesModule() throws IOException {
         Module.setModuleType(ModuleType.games);
         SceneController.goToTopicList();
     }
