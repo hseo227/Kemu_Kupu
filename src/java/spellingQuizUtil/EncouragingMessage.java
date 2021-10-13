b8af0cd1cce4fc062104fc12a9e6e76fa96916a4
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-10-14 04:03:43 +1300

    Add javadoc for EncouragingMessage and FestivalSpeech

--- src/java/spellingQuizUtil/EncouragingMessage.java
+++ src/java/spellingQuizUtil/EncouragingMessage.java
@@ -7,8 +7,13 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class EncouragingMessage {
-    ArrayList<String> messageList = new ArrayList<String>();  // all the messages in the file
+    ArrayList<String> messageList = new ArrayList<>();  // all the messages in the file
 
+
+    /**
+     * Setting up the encouraging message list
+     * @param result Encouraging messages for that specific result
+     */
     public EncouragingMessage(String result) {
         // get all the messages in the file
         String fileName = "messages/" + result;
@@ -28,7 +33,11 @@ public class EncouragingMessage {
             e.printStackTrace();
         }
     }
-    
+
+    /**
+     * Get a random message in the message list
+     * @return A random encouraging message
+     */
     public String getEncourageMsg() {
         // get random message
         Random rand = new Random();
