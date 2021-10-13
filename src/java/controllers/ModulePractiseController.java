50c46c1f27d85cbc6c147353f3310bf8803afc47
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-10-14 00:50:55 +1300

    a

--- src/java/controllers/ModulePractiseController.java
+++ src/java/controllers/ModulePractiseController.java
@@ -1,11 +1,15 @@
 package controllers;
 
-
 import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
