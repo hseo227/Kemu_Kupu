2b6622ab2dc9fde1a9ab8a93e3da528ff1206a64
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-10-17 22:39:07 +1300

    Reward screen table display accurately

--- src/java/controllers/RewardScreenController.java
+++ src/java/controllers/RewardScreenController.java
@@ -5,16 +5,20 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import spellingQuiz.Module;
 import spellingQuizUtil.ModuleType;
 import spellingQuizUtil.Result;
 import spellingQuizUtil.Score;
+import spellingQuizUtil.Statistics;
 import tableUtil.TableStatistics;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class RewardScreenController implements Initializable {
+    private final ArrayList<TableStatistics> statisticsList = new ArrayList<>();
 
     @FXML
     private Label userScoreLabel;
@@ -32,7 +36,22 @@ public class RewardScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         userScoreLabel.setText(Score.getScore() + " OUT OF " + Score.getTotalScore());
 
-    
+        ArrayList<String> words = Statistics.getTestedWords();
+        ArrayList<Result> results = Statistics.getWordResult();
+        ArrayList<Integer> scores = Statistics.getWordScore();
+        ArrayList<Integer> times = Statistics.getWordTime();
+
+        for (int i = 0 ; i < words.size(); i++) {
+            statisticsList.add(new TableStatistics(i + 1, words.get(i), results.get(i).name(), scores.get(i), times.get(i)));
+        }
+
+        // setting up the table and the column
+        roundCol.setCellValueFactory(new PropertyValueFactory<>("round"));
+        wordCol.setCellValueFactory(new PropertyValueFactory<>("word"));
+        resultCol.setCellValueFactory(new PropertyValueFactory<>("result"));
+        scoreCol.setCellValueFactory(new PropertyValueFactory<>("score"));
+        timeCol.setCellValueFactory(new PropertyValueFactory<>("time"));
+        table.getItems().setAll(statisticsList);
     }
     
 
